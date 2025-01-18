b51ebeabfbd505d5d2db1625839ef9b8ecb102e5
allaithbitar <allaithbitar.code0@gmail.com>
2025-01-18 12:52:21 +0300

    added trycatch to seeding script

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,20 +1,25 @@
 import { dashboardAccounts, userPreferences, users } from './schema';
 import { db } from '.';
 
-await db.insert(users).values({
-  tg_id: 535562391,
-});
+try {
+  console.log('SEEDING');
+  await db.insert(users).values({
+    tg_id: 535562391,
+  });
 
-await db.insert(userPreferences).values({
-  user: 535562391,
-  nickname: 'SolZedd',
-  user_type: 'Provider',
-});
+  await db.insert(userPreferences).values({
+    user: 535562391,
+    nickname: 'SolZedd',
+    user_type: 'Provider',
+  });
 
-await db.insert(dashboardAccounts).values({
-  password: '12345',
-  bot_user_id: 535562391,
-  role: 'Admin',
-});
+  await db.insert(dashboardAccounts).values({
+    password: '12345',
+    bot_user_id: 535562391,
+    role: 'Admin',
+  });
+} catch (error) {
+  console.warn('ALREADY SEEDED');
+}
 
 process.exit();
