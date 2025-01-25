ddad15537f5c975482a566475a006dd6be3b8621
allaithbitar <allaithbitar.code0@gmail.com>
2025-01-25 04:57:34 +0300

    added start script and enhanced docker file

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 bun run drizzle-kit migrate
-bun run start
+
+if [ "$NODE_ENV" = "production" ]; then
+  # Run the production server
+  bun run start
+else
+  # Run the development server
+  bun run dev
+fi
